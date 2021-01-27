413fbe3b4a07db67639966712fd45b28b3782c4e
Jaime <jaime.kvaternik17@gmail.com>
2021-01-27 01:45:28 -0500

    fix App.test.js

--- client/web/src/App.test.js
+++ client/web/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders datums', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/My Data/i);
   expect(linkElement).toBeInTheDocument();
 });
